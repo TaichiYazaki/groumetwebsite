964dd90ff42da479fbfc22df52cfef55beaa29dd
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2022-06-08 11:27:34 +0900

    first commit

---
+++ src/main/java/com/example/groumetwebsite/GroumetwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.groumetwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroumetwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroumetwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/groumetwebsite/GroumetwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groumetwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroumetwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
